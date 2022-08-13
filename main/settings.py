5998d2e7bee6b59a6461d88e735da3e79569a3a1
johnnylyimo <johnnylyimo@gmail.com>
2022-08-13 16:14:23 +0300

    Added tailwind theme app to the settings installed apps

--- main/settings.py
+++ main/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'memorydiary',
     'tailwind',
+    'theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

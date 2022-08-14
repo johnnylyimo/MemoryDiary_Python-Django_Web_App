a89927bb66cae976e1e280a8d263fc33dcbd090b
johnnylyimo <johnnylyimo@gmail.com>
2022-08-14 15:51:35 +0300

    Created new TrackingModel's field update_at

--- helpers/models.py
+++ helpers/models.py
@@ -1,4 +1,6 @@
+from turtle import update
 from django.db import models
 
 class TrackingModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField()

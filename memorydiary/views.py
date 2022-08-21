838cb1cf349c9677710d199cb26aa3a389fda826
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 01:59:06 +0300

    Defined new variable retrieve form data

--- memorydiary/views.py
+++ memorydiary/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-    
+        newMemory = request.POST['memory']
     else:
         return render(request, 'index.html')

965742997b6a5d2d5fe18ccf8ac63c14ef7dabb2
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 02:07:55 +0300

    Imported table model Memory

--- memorydiary/views.py
+++ memorydiary/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
+
 def home(request):
     if request.method == 'POST':
         newMemory = request.POST['memory']
         newMemory.save()
+        return redirect('home')
     else:
         return render(request, 'index.html')

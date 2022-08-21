70dc6d0ad4fc61f22eb5034caf18b56a1f53cebb
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 01:57:28 +0300

    Defined else condition that return HTML template

--- memorydiary/views.py
+++ memorydiary/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-    return render(request, 'index.html')
+    
+    else:
+        return render(request, 'index.html')

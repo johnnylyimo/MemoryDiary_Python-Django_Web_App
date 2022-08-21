fc0f128ef08ffe65c86e7c4f2ea41f79e0747365
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 02:09:30 +0300

    Created Memory instance retrieve form input

--- memorydiary/views.py
+++ memorydiary/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-
+from . models import Memory
 
 def home(request):
     if request.method == 'POST':
-        newMemory = request.POST['memory']
+        newMemory = Memory(content=request.POST['memory'])
         newMemory.save()
-        return redirect('home')
+
     else:
         return render(request, 'index.html')

5dbc8043dbfcc7647873fdd22398e10dadb85143
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 03:13:05 +0300

    Refactored coded removed unused codes

--- main/settings.py
+++ main/settings.py
@@ -32,9 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'memorydiary',
-    'tailwind',
-    'theme',
-    'django_browser_reload',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,10 +124,3 @@ STATICFILES_DIRS = ['static',]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TAILWIND_APP_NAME = 'theme'
-
-INTERNAL_IPS = [
-	"127.0.0.1",
-]
-
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"

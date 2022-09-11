24fc4ab206d5fcb8562de044d2016be855197e91
johnnylyimo <johnnylyimo@gmail.com>
2022-09-11 15:35:50 +0300

    Loaded bootstrap_icons with njinja template

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_bootstrap_icons'
+    'django_bootstrap_icons',
     'memorydiary',
     'django.contrib.admin',
     'django.contrib.auth',
